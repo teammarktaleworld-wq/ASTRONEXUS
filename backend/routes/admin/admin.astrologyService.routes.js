cdbc2e3551b27012745cc8ca3858f345cacdb63c
vaibhav joshi <vaibhavjosh0709@gmail.com>
2026-04-19 00:30:16 +0530

    New

--- backend/routes/admin/admin.astrologyService.routes.js
+++ backend/routes/admin/admin.astrologyService.routes.js
@@ -9,7 +9,7 @@ const {
   toggleService
 } = require("../../controllers/services/astrologyServiceController.js");
 const { authenticateToken } = require("../../middlewares/auth.js");
-const { adminMiddleware } = require("../../middlewares/admin.middleware.js");
+const adminMiddleware = require("../../middlewares/admin.middleware.js");
 
 // All routes are admin protected
 router.use(authenticateToken, adminMiddleware);
